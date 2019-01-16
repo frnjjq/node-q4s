a47db63f34a42d7f8050c2dd12bffad9d8bcd731
Francisco José Juan Quintanilla <frnjjq@gmail.com>
2019-01-16 21:29:13 +0100

    Refractor to Syncronous code for Req and Res. Added checking methods.

--- src/ReqQ4S.js
+++ src/ReqQ4S.js
@@ -23,105 +23,113 @@ class ReqQ4S {
     this.body = body;
   }
   /**
-   * Build a new ReqQ4S from an string. Validates the the string is correct.
-   * @param {string} str - The string to parse to the ReqQ4S.
-   * @return {Promise} On sucess (ReqQ4S) , on error (error).
+   * Factory method to build a new ReqQ4S from an string. Validates the the 
+   * created request object is correct.
+   * @param {string} message - The mesage which has to be parsed into a ReqQ4S.
+   * @return {Object} On sucess an object of the class ReqQ4S, on error returns an error.
    */
-  static fromString(str) {
-    return new Promise((resolve, reject) => {
-      let method; let requestURI; let q4sVersion;
-      const headers = {};
+  static fromString(message) {
+    let method; let requestURI; let q4sVersion;
+    const headers = {};
 
-      const body = str.substring(str.indexOf('\r\n\r\n') + 4);
-      const headerPart = str.substring(0, str.indexOf('\r\n\r\n'));
-      const headLine = headerPart.split('\r\n');
-      headLine.forEach((item, index) => {
-        if (index === 0) {
-          const values = item.split(' ');
-          method = values[0];
-          requestURI = values[1];
-          q4sVersion = values[2];
-        } else {
-          const header = item.split(': ');
-          headers[header[0]] = header[1];
-        }
-      });
-      const req = new ReqQ4S(method, requestURI, q4sVersion, headers, body);
-      req.validate()
-          .then(() => {
-            resolve(req);
-          })
-          .catch((err) => {
-            reject(err);
-          });
+    const body = str.substring(str.indexOf('\r\n\r\n') + 4);
+    const headerPart = str.substring(0, str.indexOf('\r\n\r\n'));
+    const headLine = headerPart.split('\r\n');
+    headLine.forEach((item, index) => {
+      if (index === 0) {
+        const values = item.split(' ');
+        method = values[0];
+        requestURI = values[1];
+        q4sVersion = values[2];
+      } else {
+        const header = item.split(': ');
+        headers[header[0]] = header[1];
+      }
     });
+    const req = new ReqQ4S(method, requestURI, q4sVersion, headers, body);
+    if (req.validate()) {
+      return req;
+    }
+    return new Error("Malformed request.");
   }
   /**
-   * Check that the data in this is correct and returns an error if data is malformed.
-   * @return {Promise} On sucess () , on error (error).
+   * Check that the data in this is correct and returns true if all is ok or 
+   * false if data is malformed. 
+   * @return {boolean} True is the data is correct. False otherwise.
    */
   validate() { // TODO: Improve this comprobation when you have the class clear
-    return new Promise((resolve, reject) => {
-      if (!this.method) {
-        reject(new Error('Method is not present in the request line.'));
+    if (!this.method) {
+      return false;
+    }
+    if (!(this.method === 'BEGIN' || this.method === 'READY' ||
+      this.method === 'PING' || this.method === 'Q4S-ALERT' ||
+      this.method === 'Q4S-RECOVERY' || this.method === 'CANCEL' ||
+      this.method === 'PING')) {
+      return false;
+    }
+    if (this.method === 'READY' && typeof this.headers.stage === 'undefined') {
+      return false;
+    }
+    if (!this.q4sVersion) {
+      return false;
+    }
+    if (typeof this.headers.signature !== undefined) {
+      if (crypto.createHash('md5').update(this.body).digest("hex") != this.headers.signature) {
+        return false;
       }
-      if (!(this.method === 'BEGIN' || this.method === 'READY' ||
-        this.method === 'PING' || this.method === 'Q4S-ALERT' ||
-        this.method === 'Q4S-RECOVERY' || this.method === 'CANCEL' ||
-        this.method === 'PING')) {
-        reject(new Error('Method field  does not contain a valid word.'));
-      }
-      if (this.method === 'READY' && typeof this.headers.stage === 'undefined') {
-        reject(new Error('READY method without stage'));
-      }
-      if (!this.q4sVersion) {
-        reject(new Error('Missing parameter in the request start line'));
-      }
-      if (typeof this.headers.signature !== undefined) {
-        if (crypto.createHash('md5').update(this.body).digest("hex") != this.headers.signature) {
-          reject(new Error('Signature MD5 does not match the body'));
-        }
-      }
-      if (typeof this.headers["Content-Encoding"] !== undefined) {
-        reject(new Error('Body in the request is compressed'));
-      }
-      if (typeof this.headers["Content-Type"] === undefined) {
-        reject(new Error('Content-Type header is not present'));
-      }
-      if (typeof this.headers["Transfer-Encoding"] !== undefined && this.headers.Transfer-Encoding !== 'indentity') {
-        reject(new Error('Transfer-Encoding can only be identity'));
-      }
-      if (!this.requestURI) {
-        reject(new Error('Request Uri is not present in the request line.'));
-      }
-      resolve();
-    });
+    }
+    if (typeof this.headers["Content-Encoding"] !== undefined) {
+      return false;
+    }
+    if (typeof this.headers["Content-Type"] === undefined) {
+      return false;
+    }
+    if (typeof this.headers["Transfer-Encoding"] !== undefined && this.headers.Transfer - Encoding !== 'indentity') {
+      return false;
+    }
+    if (!this.requestURI) {
+      return false;
+    }
+    return true;
   }
 
   /**
    * Introduces a new header signature in this containing the MD5 of the body of this.
    */
   signBody() {
-    this.headers.signature = crypto.createHash('md5').update(this.body).digest("hex") 
+    this.headers.signature = crypto.createHash('md5').update(this.body).digest("hex")
   }
+
   /**
-   * Returns a promise with the string format of this Request class. Use to send this request.
-   * @return {promise} -  On sucess (string) , on error (error).
+   * Form the message format of this request. Use it to send it over TCP/UDP.
+   * @return {string} -  String format of this response.
    */
   toString() {
-    return new Promise((resolve, reject) => {
-      let message = '';
-      message = message + this.method + ' ' + this.requestURI + ' ';
-      message = message + this.q4sVersion + '\r\n';
+    let message = '';
+    message = message + this.method + ' ' + this.requestURI + ' ';
+    message = message + this.q4sVersion + '\r\n';
 
-      const entries = Object.entries(this.headers);
-      entries.forEach((item) => {
-        message = message + item[0] + ': ' + item[1] + '\r\n';
-      });
-      message = message + '\r\n';
-      message = message + this.body;
-      resolve(message);
+    const entries = Object.entries(this.headers);
+    entries.forEach((item) => {
+      message = message + item[0] + ': ' + item[1] + '\r\n';
     });
+    message = message + '\r\n';
+    message = message + this.body;
+    return message;
+  }
+
+    /**
+   * Static class method that allows to check whether a string is a Request.
+   * it is intended for fast checking as validity is not a concern.
+   * @return {boolean} -  True if it is a Response, False if it is not
+   */
+  static isRequest(message) {
+    const startLine = str.substring(0, message.indexOf('\r\n'));
+    const values = startLine.split(' ');
+    if (parseInt(values[1], 10) !== NaN) {
+      return true;
+    }
+    return false;
   }
 };
 
--- src/ResQ4S.js
+++ src/ResQ4S.js
@@ -22,70 +22,79 @@ class ResQ4S {
     this.body = body;
   }
   /**
-   * Build a new ResQ4S from an string. Validates the the string is correct.
+   * Factory method to build a new ResQ4S from an string. Validates the the 
+   * string is correct.
    * @param {string} str - The string to parse to the ReqQ4S.
-   * @return {Promise} On sucess (ResQ4S) , on error (error).
+   * @return {Object} On sucess an object of the class ResQ4S, on error returns an error.
    */
   static fromString(str) {
-    return new Promise((resolve, reject) => {
-      let q4sVersion; let statusCode; let reasonPhrase;
-      const headers = {};
+    let q4sVersion; let statusCode; let reasonPhrase;
+    const headers = {};
 
-      const body = str.substring(str.indexOf('\r\n\r\n') + 4);
-      const headerPart = str.substring(0, str.indexOf('\r\n\r\n'));
-      const headLine = headerPart.split('\r\n');
-      headLine.forEach((item, index) => {
-        if (index === 0) {
-          const values = item.split(' ');
-          q4sVersion = values[0];
-          statusCode = values[1]; // TODO:Add a number cast to store it as a number from string
-          reasonPhrase = values[2];
-        } else {
-          const header = item.split(': ');
-          headers[header[0]] = header[1];
-        }
-      });
-      const res = new ResQ4S(q4sVersion, statusCode, reasonPhrase, headers, body);
-      res.validate()
-          .then(() => {
-            resolve(req);
-          })
-          .catch((err) => {
-            reject(err);
-          });
+    const body = str.substring(str.indexOf('\r\n\r\n') + 4);
+    const headerPart = str.substring(0, str.indexOf('\r\n\r\n'));
+    const headLine = headerPart.split('\r\n');
+    headLine.forEach((item, index) => {
+      if (index === 0) {
+        const values = item.split(' ');
+        q4sVersion = values[0];
+        statusCode = values[1]; // TODO:Add a number cast to store it as a number from string
+        reasonPhrase = values[2];
+      } else {
+        const header = item.split(': ');
+        headers[header[0]] = header[1];
+      }
     });
+    const res = new ResQ4S(q4sVersion, statusCode, reasonPhrase, headers, body);
+    if (res.validate()) {
+      return res;
+    }
+    return new Error("Malformed response.");
   }
+
   /**
-   * Check that the data is correct and follows the required rules.
-   * @return {Promise} On sucess () , on error (error).
+   * Check that the data is correct and follows the required rules. This 
+   * method checks the response, so there wont be body validation.
+   * @return {boolean} True is the data is correct. False otherwise.
    */
   validate() { // TODO: Improve this comprobation when you have the class clear
-    return new Promise((resolve, reject) => {
-      // TODO -> Content-Type debe de estar en todas las request que se manden
-      // TODO -> "Accept-Encoding" header debe de ser "identity" forzado
-      // TODO -> Si es chunqued me lo cargo
-      resolve();
-    });
+    // TODO -> Content-Type debe de estar en todas las request que se manden
+    // TODO -> "Accept-Encoding" header debe de ser "identity" forzado
+    // TODO -> Si es chunqued me lo cargo
+    return true;
   }
+
   /**
-   * Returns a promise with the string format of this object.
-   * @return {promise} -  On sucess (string) , on error (error).
+   * Form the message format of this Response and send it over the network.
+   * @return {string} -  String format of this Request.
    */
   toString() {
-    return new Promise((resolve, reject) => {
-      let message = '';
-      // TODO: Cast to string ¿automatically?
-      message = message + this.q4sVersion + ' ' + this.statusCode + ' ';
-      message = message + this.reasonPhrase + '\r\n';
+    let message = '';
 
-      const entries = Object.entries(this.headers);
-      entries.forEach((item) => {
-        message = message + item[0] + ': ' + item[1] + '\r\n';
-      });
-      message = message + '\r\n';
-      message = message + this.body;
-      resolve(message);
+    message = message + this.q4sVersion + ' ' + this.statusCode + ' ';
+    message = message + this.reasonPhrase + '\r\n';
+
+    const entries = Object.entries(this.headers);
+    entries.forEach((item) => {
+      message = message + item[0] + ': ' + item[1] + '\r\n';
     });
+    message = message + '\r\n';
+    message = message + this.body;
+    return message;
+  }
+
+  /**
+   * Static class method that allows to check whether a string is a Response.
+   * it is intended for fast checking as validity is not a concern.
+   * @return {boolean} -  True if it is a Response, False if it is not
+   */
+  static isResponse(message) {
+    const startLine = str.substring(0, message.indexOf('\r\n'));
+    const values = startLine.split(' ');
+    if (parseInt(values[1], 10) !== NaN) {
+      return true;
+    }
+    return false;
   }
 };
 
